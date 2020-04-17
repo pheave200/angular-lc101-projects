58a00abb215a1b66ea6dc8c9c01c588451d7cd1c
Heavey <pheave200@cable.comcast.com>
2020-04-17 14:10:07 -0400

    updated

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Random Images';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://www.jpl.nasa.gov/spaceimages/images/mediumsize/PIA10364_ip.jpg';
+  image3 = 'http://justfunfacts.com/wp-content/uploads/2015/11/volcanic-eruption-lightning.jpg';
 
   constructor() { }
 
